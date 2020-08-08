89d53b7c0c9ab58921666d798299d8621ea355e9
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-08 18:37:41 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,14 +5,20 @@
 
 class EmailParser
 
-  attr_accessor :email
-  def  initialize(emails)
-    @email = emails
+  attr_accessor :email    #email accessor instance method defined as 'email'
+  def  initialize(emails)  #defining initialize to must run email as argumnt
+    @email = emails    #assign email accessor method to an instance variable 
   end
 
-  def parse
-    email_array = @email.split(/[, ]/).uniq
-    email_array.reject! {|element| element.empty?}
+  def parse   #defining instance method with 'parse'
+    email_array = @email.split(/[, ]/).uniq  
+    
+        #sets a constant 'email_array' to the @email instance variable, splitting every unique instance into its own string, and then putting it in an array
+    
+    email_array.reject! {|element| element.empty?}   
+    
+        #  .reject! Deletes every element of self for which the block evaluates to true, if no changes were made returns nil.
+    
     email_array
   end
 
